86c1c59220909091abd7ac86c981aafda4d927c4
Elar Saks <elarsaks@gmail.com>
2023-12-30 01:13:09 +0200

    Format code.

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 # Instance type (aws_instance) and name (terraform-demo)
 resource "aws_instance" "terraform-demo" {
   # You can free-tier eligible AMIs on the AWS web console.
-  ami           = "ami-027ae750a83e2c590" 
+  ami           = "ami-027ae750a83e2c590"
   instance_type = "t3.micro"
 }
 
